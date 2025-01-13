95e374caf3f1bdb7abe297517aa402f22db19c45
Kolotysha Dmitrij <suvstreet@gmail.com>
2025-01-13 22:27:31 +0300

    feat(config): Настроить Source Maps.

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -2,5 +2,6 @@ const { merge } = require('webpack-merge')
 const commonConfig = require('./webpack.config.common')
 
 module.exports = merge(commonConfig, {
-  mode: 'production'
+  mode: 'production',
+  devtool: 'source-map',
 })
