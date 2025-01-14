34f4f20c65fb5d2ebf606cea79a0a59f7821a009
Kolotysha Dmitrij <suvstreet@gmail.com>
2025-01-14 18:42:28 +0300

    feat(package, config): Подключить локальный сервер

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -4,4 +4,9 @@ const commonConfig = require('./webpack.config.common')
 module.exports = merge(commonConfig, {
   mode: 'development',
   devtool: 'inline-source-map',
+  devServer: {
+    open: true,
+    port: 8080,
+    hot: true,
+  },
 })
