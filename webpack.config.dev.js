63e2cf05fc7a623155ce7d1021db14b4c947e523
Kolotysha Dmitrij <suvstreet@gmail.com>
2025-01-15 19:32:35 +0300

    fix(config): Подключить ESLintPlugin

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,4 +1,5 @@
 const { merge } = require('webpack-merge')
+const ESLintPlugin = require('eslint-webpack-plugin')
 const commonConfig = require('./webpack.config.common')
 
 module.exports = merge(commonConfig, {
